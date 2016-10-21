98359a6a030c968900c979dbe4e5afa751793ad0
Anton Lomakin <lomakin.antonio@Yandex.ru>
2016-10-21 08:20:07 +0500

    -

--- get-chat-android/app/src/main/java/com/example/get_chat_android/MainActivity.java
+++ get-chat-android/app/src/main/java/com/example/get_chat_android/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.get_chat_android;
 
 import android.content.Intent;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,24 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button b1 = (Button) findViewById(R.id.buttonOk);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // Log.e("MainActivity", "Hello world!");
-            }
-        });
-
-
-
         Button b2 = (Button) findViewById(R.id.btRegistraion);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
-
             }
         });
 
+        buildObservable()
+                .doOnNext(new Action1<News>() {
+                    @Override
+                    public void call(News news) {
+                        Log.i("MainActivity", "title:" + news.title + ", text:" + news.text);
+                    }
+                }).subscribe();
+
+
+    }
+
+    Observable<News> buildObservable() {
+
+
+        return Observable.create(new Observable.OnSubscribe<News>() {
+            @Override
+            public void call(Subscriber<? super News> subscriber) {
+                News news = new News("TItle", "Text news");
+                subscriber.onNext(news);
+                subscriber.onCompleted();
+            }
+        });
     }
 }
