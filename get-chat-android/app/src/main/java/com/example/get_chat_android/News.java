b33f39c700c70291d8c359af4ec0e0109682b50c
Anton Lomakin <lomakin.antonio@Yandex.ru>
2016-10-21 08:20:38 +0500

    -

---
+++ get-chat-android/app/src/main/java/com/example/get_chat_android/News.java
@@ -0,0 +1,15 @@
+package com.example.get_chat_android;
+
+/**
+ * Created by Антон on 16.10.2016.
+ */
+
+public class News {
+    public String title;
+    public String text;
+
+    public News(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+}
